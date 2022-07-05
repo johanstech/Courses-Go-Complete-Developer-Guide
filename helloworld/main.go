bf62f01f3b8373d3cb372edfb8c0c5a9aeb0149e
Johan Andersson <johanstechs@gmail.com>
2022-07-05 11:52:52 +0200

    Basic hello world program and Go explanation

---
+++ helloworld/main.go
@@ -0,0 +1,9 @@
+//? main is for executables, any other name is for reusable
+package main
+
+import "fmt"
+
+//? executables always need a func main()
+func main() {
+	fmt.Println("Hello world!")
+}
