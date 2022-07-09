b767c2aacaa8c59ed5aed460b55c2b23644642f0
Johan Andersson <johanstechs@gmail.com>
2022-07-09 20:47:45 +0200

    Added info about variables and walrus operator

--- helloworld/main.go
+++ helloworld/main.go
@@ -5,5 +5,11 @@ import "fmt"
 
 //? executables always need a func main()
 func main() {
-	fmt.Println("Hello world!")
+	//* Long way of declaring variable
+	// var message string = "Hello world!"
+
+	//* Walrus operator := is only used when declaring a variable, not reassigning
+	message := "Hello world!"
+
+	fmt.Println(message)
 }
