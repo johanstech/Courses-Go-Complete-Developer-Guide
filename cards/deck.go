78610c614c65785b48de0648e6162971ef19c93f
Johan Andersson <johanstechs@gmail.com>
2022-07-09 21:11:39 +0200

    Added newDeck function

--- cards/deck.go
+++ cards/deck.go
@@ -1,5 +1,28 @@
 package main
 
-//* create a new type of 'deck'
-//* which is a slice of strings []string
+import "fmt"
+
+// create a new type of 'deck'
+// which is a slice of strings []string
 type deck []string
+
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Clubs", "Hearts"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Knight", "Queen", "King"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+func (d deck) print() {
+	for i, card := range d {
+		fmt.Println(i, card)
+	}
+}
--- cards/main.go
+++ cards/main.go
@@ -1,23 +1,7 @@
 package main
 
-import "fmt"
-
 func main() {
-	//? Long way of declaring variable
-	// var card string = "Ace of Spades"
-	//? Walrus operator := is only used when declaring a variable, not reassigning
-	// card := newCard()
+	cards := newDeck()
 
-	cards := deck{newCard(), "Ace of Hearts"}
-	cards = append(cards, "Six of Spades")
-
-	fmt.Println(cards)
-
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-}
-
-func newCard() string {
-	return "Five of Diamonds"
+	cards.print()
 }
