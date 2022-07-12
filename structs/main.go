9007e806a5d67ae630dfe0a9c2c2d2589fcaa670
Johan Andersson <johanstechs@gmail.com>
2022-07-12 04:21:21 +0200

    Pointers 1

--- structs/main.go
+++ structs/main.go
@@ -2,17 +2,39 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// person1 := person{"Bob", "Bobson"}
-	person1 := person{firstName: "Bob", lastName: "Bobson"}
-	fmt.Println(person1)
+	person1 := person{
+		firstName: "Bob",
+		lastName:  "Bobson",
+		contact: contactInfo{
+			email:   "bob.bobson@mail.com",
+			zipCode: 12345,
+		},
+	}
+	person1.print()
 
-	person1.firstName = "Bobbiest"
+	person1Pointer := &person1
+	person1Pointer.updateName("Bobbiest")
 	person1.lastName = "Boblington"
-	fmt.Printf("%+v", person1)
+	person1.print()
+}
+
+func (p person) print() {
+	fmt.Printf("%+v\n", p)
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
